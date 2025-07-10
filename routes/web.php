3758b9f18196c6438f4e4c88f828f02a253a8fc9
lami <dorella.mphan@gmail.com>
2025-07-10 17:51:01 +0200

    "Routing" Part

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,63 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
-    $person = [
-        "name" => "phandor",
-        "email" => "phan.dorel.lam@gmail.com",
-    ];
-    dd($person);
+//    $aboutPageUrl = '/about';
+//    $aboutPageUrl = route('about');
+//    dd($aboutPageUrl);
+
+//    $productUrl = route('product.view', ['lang' => 'en', 'id' => 1]);
+//    dd($productUrl);
+
     return view('welcome');
 });
 
-Route::view('/about', 'about');
+Route::view('/about', 'about')->name('about');
 
-Route::get('/product/{category?}', action: function(string $category = null) {
-    return "Product for category=$category";
-}); 
+Route::prefix('admin')->group(function () {
+    Route::get('/users', function () {
+        return '/admin/users';
+    });
+});
+
+//Route::get('/user/profile', function(){})->name('profile');
+
+//Route::get('/current-user', function(){
+//    return redirect()->route('profile');
+//    return to_route('profile');
+//});
+
+//Route::get("/p/{lang}/{id}", function(string $lang, string $id){
+//    // ...
+//})->name("product.view");
+
+
+//Route::get('/', function () {
+//    $person = [
+//        "name" => "phandor",
+//        "email" => "phan.dorel.lam@gmail.com",
+//    ];
+//    dd($person);
+//    return view('welcome');
+//});
+
+//Route::get('/user/{username}', function(string $username){
+//    // ...
+//})->where('username', '[a-z]+');
+//
+//
+//Route::get("{lang}/product/{id}", function(string $lang, string $id){
+//    // ...
+//})->where(['lang' => '[a-z]{2}', 'id' => '\d{4,}']);
+//
+//Route::get('/search/{search}', function (string $search) {
+//    return $search;
+//})->where('search', '.+'); 
+
+//Route::get('/product/{category?}', function(?string $category = null) {
+//    return "Product for category=$category";
+//});
 
 //Route::get('/product/{id}', function(string $id) {
 //    return "Product ID=$id";
